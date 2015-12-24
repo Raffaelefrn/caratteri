54efaa74c1b1200cd2a8fb2f0a4567fdbddf12b6
Raffaele-PC <raffa.fari57@gmail.com>
2015-12-24 20:00:26 +0100

    aggiornato elimina spazi multipli

--- elimina_spazi_multipli.c
+++ elimina_spazi_multipli.c
@@ -6,10 +6,12 @@ int main()
 
   while ( (c=getchar()) != EOF) {
 if (c==' '){conta++;}
-if (conta==2){c=0;
-conta=0;}
-if (c!=' '){conta=0;}
+if (conta==2){
+c=0;
+conta=1;}
+if ((c!=' ')&&(c!=0)){conta=0;}
 putchar(c);
 }
+
 return 0;
 }
