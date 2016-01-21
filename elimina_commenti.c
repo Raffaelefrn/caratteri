73aae80ae5a03cc465bcad5e2d3ad5006e5e0e2a
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-01-21 11:41:08 +0100

    Update elimina_commenti.c

--- elimina_commenti.c
+++ elimina_commenti.c
@@ -6,13 +6,14 @@ int main()
   int c,stato=testo;
 
   while ( (c=getchar()) != EOF) {
-if (c!='*' && stato==slash){
-							printf("/");
-							stato=testo;
-						}
-
-if (c=='/' && stato==testo){stato=slash;}
-if(c=='*' && stato==slash){stato=asterisco;}
+    if (c!='*' && stato==slash) {
+      printf("/");
+      stato=testo;
+    } else if (c=='/' && stato==testo) {
+      stato=slash;
+    } else if (c=='*' && stato==slash) {
+      stato=asterisco;
+    }
 if(stato==testo){putchar(c);}
 if(c=='/' && stato==asterisco){stato=testo;}
 
